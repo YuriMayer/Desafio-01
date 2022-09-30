c4f7da549deaa36dcd566bdd3d49639eff2f1f5c
yuri <yurimayer@hotmail.com>
2022-09-30 14:12:41 -0300

    Desafio 01 concluído.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const newTask : Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    // console.log(newTask);
+
+    setTasks([...tasks, newTask])
     //TODO - add new task
   }
 
   function handleToggleTaskDone(id: number) {
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const editedTask = updatedTasks.find(task => task.id === id) 
+
+    if(!editedTask)
+      return;
+    
+    editedTask.done = !editedTask.done;
+
+    setTasks(updatedTasks)
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
+    setTasks(tasks.filter(task => task.id !== id))
     //TODO - remove task from state
   }
 
